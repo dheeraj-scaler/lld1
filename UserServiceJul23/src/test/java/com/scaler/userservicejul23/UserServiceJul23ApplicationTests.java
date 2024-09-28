9514ce566c9b3614eca66123a6fc92afea3aa7ed
Default User <defaultuser@Defaults-Air.bbrouter>
2024-09-28 18:03:55 +0530

    Auth - 4

--- UserServiceJul23/src/test/java/com/scaler/userservicejul23/UserServiceJul23ApplicationTests.java
+++ UserServiceJul23/src/test/java/com/scaler/userservicejul23/UserServiceJul23ApplicationTests.java
@@ -1,13 +1,44 @@
 package com.scaler.userservicejul23;
 
+import com.scaler.userservicejul23.security.repositories.JpaRegisteredClientRepository;
+import org.antlr.v4.runtime.misc.LogManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserServiceJul23ApplicationTests {
+
+    @Autowired
+    private JpaRegisteredClientRepository registeredClientRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void addRegisteredClientToDB() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oidc-client")
+                .clientSecret("$2a$12$vJHFJt0bk7F6dR.jvD/E5.jOk7eXnFqtYAdVqPL2GZhH0zcgVYaQm")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
+                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("ADMIN")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        registeredClientRepository.save(registeredClient);
+    }
 }
